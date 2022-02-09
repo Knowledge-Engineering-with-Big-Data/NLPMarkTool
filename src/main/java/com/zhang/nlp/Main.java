bc769a055d73de3201551b318baea665c53855b4
brillience <360727078@qq.com>
2022-02-09 16:18:57 +0800

    fix threads

--- src/main/java/com/zhang/nlp/Main.java
+++ src/main/java/com/zhang/nlp/Main.java
@@ -3,7 +3,6 @@ package com.zhang.nlp;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.*;
 
 public class Main {
 
@@ -17,36 +16,17 @@ public class Main {
         props.setProperty("depparse.model", "edu/stanford/nlp/models/parser/nndep/english_UD.gz");
         props.setProperty("parse.maxlen", "100");
         props.setProperty("ssplit.boundaryTokenRegex", "[.]|[!?]+|[。]|[！？]+");
+        props.setProperty("threads","2");
 
         ArrayList<List<String>> articles = TsvParser.getTsv("./articles.tsv");
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(6, 8, 0,TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(512),new ThreadPoolExecutor.CallerRunsPolicy());
-//        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        // build pipeline
         DocunmentParse docunmentParse = new DocunmentParse(props);
+
         for (List<String> article : articles) {
-//            ArrayList<Sentence> parseResults = docunmentParse.getParseResults(article.get(0), Utils.cleanTxt(article.get(1)));
-//            TsvParser.saveToTsv(parseResults,"./sentences.tsv");
-//            taskDoneNum++;
-//            System.out.println("[INFO] 第 "+Integer.toString(taskDoneNum)+" 篇文献标记完成！");
-//            executorService.execute(() -> {
-            threadPoolExecutor.execute(() -> {
-                String threadInfo = "[ Thread : " + Thread.currentThread().getId() + " ]";
-                try {
-                    ArrayList<Sentence> parseResults = docunmentParse.getParseResults(article.get(0), Utils.cleanTxt(article.get(1)));
-                    TsvParser.saveToTsv(parseResults, "./sentences.tsv");
-                    System.out.println(threadInfo + " [INFO] " + article.get(0) + " 文献标记完成！");
-                } catch (Exception e) {
-                    System.out.println("[ERROR] " + article.get(0) + " 失败！");
-                    e.printStackTrace();
-                }
-            });
+            ArrayList<Sentence> parseResults = docunmentParse.getParseResults(article.get(0), Utils.cleanTxt(article.get(1)));
+            TsvParser.saveToTsv(parseResults, "./sentences.tsv");
+            System.out.println( " [INFO] " + article.get(0) + " 文献标记完成！");
+
         }
-        threadPoolExecutor.shutdown();
-        while (true) {
-            if (threadPoolExecutor.isTerminated()) {
-                System.out.println("SUCCESS!");
-                break;
-            }
-        }
+        System.out.println("SUCCESS!");
     }
 }
