48e351a1f9806e2514a474517ae43a910c216533
brillience <360727078@qq.com>
2022-03-30 12:20:07 +0800

    fix log & file input

--- src/main/java/com/zhang/nlp/Main.java
+++ src/main/java/com/zhang/nlp/Main.java
@@ -1,11 +1,21 @@
 package com.zhang.nlp;
 
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.log4j.Logger;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.*;
 
+/**
+ * @author zhang
+ */
 public class Main {
 
+    public static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) throws IOException{
         // set up pipeline properties
         Properties props = new Properties();
@@ -16,17 +26,22 @@ public class Main {
         props.setProperty("depparse.model", "edu/stanford/nlp/models/parser/nndep/english_UD.gz");
         props.setProperty("parse.maxlen", "100");
         props.setProperty("ssplit.boundaryTokenRegex", "[.]|[!?]+|[。]|[！？]+");
-        props.setProperty("threads","2");
+        props.setProperty("threads","64");
 
-        ArrayList<List<String>> articles = TsvParser.getTsv("./articles.tsv");
-        DocunmentParse docunmentParse = new DocunmentParse(props);
-
-        for (List<String> article : articles) {
-            ArrayList<Sentence> parseResults = docunmentParse.getParseResults(article.get(0), Utils.cleanTxt(article.get(1)));
-            TsvParser.saveToTsv(parseResults, "./sentences.tsv");
-            System.out.println( " [INFO] " + article.get(0) + " 文献标记完成！");
-
+        LineIterator iterator = FileUtils.lineIterator(new File("./articles.tsv"), "UTF-8");
+        while (iterator.hasNext()){
+            String line = iterator.nextLine();
+            StringReader reader = new StringReader(line);
+            ArrayList<List<String>> articles = TsvParser.getTsv(reader);
+            DocunmentParse docunmentParse = new DocunmentParse(props);
+            for (List<String> article : articles) {
+                ArrayList<Sentence> parseResults = docunmentParse.getParseResults(article.get(0), Utils.cleanTxt(article.get(1)));
+                TsvParser.saveToTsv(parseResults, "./sentences.tsv");
+                logger.info(article.get(0) + " 文献标记完成！");
+            }
         }
-        System.out.println("SUCCESS!");
+        iterator.close();
+
+        logger.info("SUCCESS!");
     }
 }
